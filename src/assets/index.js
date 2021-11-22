4e795b52b4031aab02e1d9cc2c9aaf6e064ed220
Mounir El-Abbas <eamounir1@gmail.com>
2021-11-22 14:18:59 -0500

    Icons and images for homepage

---
+++ src/assets/index.js
@@ -0,0 +1,16 @@
+//Images
+import HeroImg_Main from "./images/homeHero-mainImg.jpg";
+import HeroImg_Accent from "./images/homeHero-accentImg.jpg";
+import FeaturesImg from "./images/homeFeaturesImg.jpg";
+import LogoIpsum from "../assets/images/logoipsum.svg";
+
+//Icons
+export { GiCompass, GiStairsGoal, GiArchiveRegister } from "react-icons/gi";
+export { AiOutlineStar } from "react-icons/ai";
+export { BsLightningFill, BsTwitter, BsFacebook } from "react-icons/bs";
+export { FiUser } from "react-icons/fi";
+export { FaHeart, FaGooglePlusG } from "react-icons/fa";
+export { IoIosBusiness } from "react-icons/io";
+
+//Export Images
+export { LogoIpsum, HeroImg_Main, HeroImg_Accent, FeaturesImg };

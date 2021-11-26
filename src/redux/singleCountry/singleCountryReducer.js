d2e32ef8e3f97c606a52a0e194c2de15e11b4c76
Mounir El-Abbas <eamounir1@gmail.com>
2021-11-26 14:15:58 -0500

    Change filter to find - Handle URL navigation on City.js

--- src/redux/singleCountry/singleCountryReducer.js
+++ src/redux/singleCountry/singleCountryReducer.js
@@ -1,9 +1,16 @@
-import { ADD_CHOSEN_COUNTRY, GET_COUNTRY_INFO } from "./singleCountryTypes";
+import {
+  ADD_CHOSEN_COUNTRY,
+  GET_COUNTRY_INFO,
+  SET_CURRENT_CITY,
+  GET_CURRENT_CITY_INFO,
+} from "./singleCountryTypes";
 import data from "../../assets/data";
+
 const initialState = {
   loading: true,
-  countryObj: [],
-  countryInfo: [],
+  countryObj: {},
+  countryInfo: {},
+  currentCityObj: {},
 };
 
 const reducer = (state = initialState, action) => {
@@ -15,14 +22,42 @@ const reducer = (state = initialState, action) => {
       };
 
     case GET_COUNTRY_INFO:
-      const countryAdditionalInfo = data.filter(
+      const countryAdditionalInfo = data.find(
         (countries) => countries.country === action.payload
       );
+      console.log(`countryAdditionalInfo`, countryAdditionalInfo);
       return {
         ...state,
         countryInfo: countryAdditionalInfo,
         loading: false,
       };
+
+    /* The following 2 cases essentially do the same thing
+      The difference is whether the user navigated to this page by clicks or url
+    */
+    //Navigating the website by clicks
+    case SET_CURRENT_CITY:
+      return {
+        ...state,
+        currentCityObj: action.payload,
+      };
+    //Navigating manually by URL
+    case GET_CURRENT_CITY_INFO:
+      let cityInfo = {};
+      for (const country of data) {
+        country.cities.forEach((city) => {
+          if (city.cityName === action.payload) {
+            return (cityInfo = city);
+          }
+        });
+      }
+
+      return {
+        ...state,
+        loading: false,
+        currentCityObj: cityInfo,
+      };
+
     default:
       return state;
   }

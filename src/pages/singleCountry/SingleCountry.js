94d3ffbe90a8d8882b53337429312953e306ef32
Mounir El-Abbas <eamounir1@gmail.com>
2021-11-26 14:10:01 -0500

    Link to city page

--- src/pages/singleCountry/SingleCountry.js
+++ src/pages/singleCountry/SingleCountry.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useLocation } from "react-router";
 import EachCountry from "../allCountries/EachCountry";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import {
   singleCountryActions,
   countriesActions,
@@ -14,24 +15,33 @@ const SingleCountry = () => {
   const countries = useSelector((state) => state.countries);
   const api = useSelector((state) => state.api);
   const dispatch = useDispatch();
-  console.log(location);
 
   /* If the user navigates to countries/:countryName in the URL  */
 
   useEffect(() => {
     if (countries.data.length) return;
+    console.log("useeffect 1");
     dispatch(apiActions.fetchCountries());
   }, []);
 
   useEffect(() => {
-    if (!api.data.length || countries.data.length) return;
+    if (!api.data.length) return;
+    console.log("useeffect 2");
+
     dispatch(countriesActions.getCountries(api.data));
   }, [api.data]);
 
   /*******************************************/
+  /* 
+if countries.data.length is full, return
+if !countries.data.length (empty) or countryInfo.length 
+*/
 
   useEffect(() => {
     if (!countries.data.length) return;
+    console.log("useeffect 3");
+    // if (Object.keys(singleCountry.countryInfo).length)
+    //   return;
     let countrySelected = location.pathname.split("/")[2]; // COUNTRY NAME: String
 
     /* If the country name has a space E.G. United States */
@@ -39,15 +49,17 @@ const SingleCountry = () => {
       countrySelected = countrySelected.split("%20").join(" ");
     }
 
-    const countrySelectedObj = countries.data.filter(
+    const countrySelectedObj = countries.data.find(
       (country) => country.name.common === countrySelected
     );
-    /* 
-    We Got country Obj - Now: dispatch the obj to state, get country information from data.js (cities, trends)
-    */
-    dispatch(countriesActions.handleToggleLayout("alternative")); //On single country page, always alternative view of country card
-    dispatch(singleCountryActions.addCountryChosenToState(countrySelectedObj)); //Pass obj to state
-    dispatch(singleCountryActions.getCountryInfo(countrySelected)); //Pass countryName:string, then iterate assets/data.js based on country name, to find additional country info (cities, trends)
+    console.log(`countrySelectedObj`, countrySelectedObj);
+
+    //On single country page, always alternative view of country card
+    dispatch(countriesActions.handleToggleLayout("alternative"));
+    //Pass obj to state
+    dispatch(singleCountryActions.addCountryChosenToState(countrySelectedObj));
+    //Pass countryName:string, then iterate assets/data.js based on country name, to find additional country info (cities, trends)
+    dispatch(singleCountryActions.getCountryInfo(countrySelected));
 
     //Now Loading is false
   }, [countries.data]);
@@ -59,24 +71,25 @@ const SingleCountry = () => {
       ) : (
         <main>
           <header>HERO</header>
-          <EachCountry country={singleCountry.countryObj[0]} />
+          <EachCountry country={singleCountry.countryObj} />
           <section className="country-trends">
-            <img
-              src={singleCountry.countryInfo[0].trends.frameworkTrend}
-              alt=""
-            />
-            <img
-              src={singleCountry.countryInfo[0].trends.languageTrend}
-              alt=""
-            />
+            <img src={singleCountry.countryInfo.trends.frameworkTrend} alt="" />
+            <img src={singleCountry.countryInfo.trends.languageTrend} alt="" />
           </section>
           <section className="country-cities">
-            {singleCountry.countryInfo[0].cities.map((city) => {
+            {singleCountry.countryInfo.cities.map((city) => {
               return (
-                <article>
-                  <h4>{city.cityName}</h4>
-                  <img src={city.images[0]} alt="" />
-                </article>
+                <Link
+                  to={`${city.cityName}`}
+                  onClick={() =>
+                    dispatch(singleCountryActions.setCurrentCity(city))
+                  }
+                >
+                  <article>
+                    <h4>{city.cityName}</h4>
+                    <img src={city.images[0]} alt="" />
+                  </article>
+                </Link>
               );
             })}
           </section>

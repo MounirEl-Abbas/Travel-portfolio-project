33d17e1e7dbb9bae0d4d8c987cc627a9a7280a9d
Mounir El-Abbas <eamounir1@gmail.com>
2021-11-28 13:37:50 -0500

    Fix 'no countries match search' message

--- src/components/allCountries-Page/AllCountriesPage.js
+++ src/components/allCountries-Page/AllCountriesPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
+/* Redux */
 import { useSelector, useDispatch } from "react-redux";
 import { apiActions, countriesActions } from "../../redux";
+/* Components */
 import FilterButtons from "./partials/FilterButtons";
 import AllCountriesContainer from "./partials/AllCountriesContainer";
 
@@ -9,38 +11,39 @@ const AllCountriesPage = () => {
   const api = useSelector((state) => state.api);
   const dispatch = useDispatch();
 
+  //Initial Render, get countries
   useEffect(() => {
     dispatch(apiActions.fetchCountriesAPI());
   }, []);
+  //Once Fetch API is completed, get Countries in Countries Reducer
   useEffect(() => {
     if (!api.countriesData.length) return;
-
     dispatch(countriesActions.getCountries(api.countriesData));
   }, [api.countriesData]);
 
-  /* Countries are Rendered conditionally */
+  //Search Bar Functionality
   let countriesToDisplay = countries.data;
-
+  //If search bar is NOT empty, filter countries
   if (countries.searchTerm) {
-    //Search bar NOT empty
     countriesToDisplay = countries.data.filter(
       (country) =>
         country.name.common.toLowerCase().indexOf(countries.searchTerm) !== -1
     );
   } else {
-    //Seach bar empty
+    //Seach bar empty, use all countries data
     countriesToDisplay = countries.data;
   }
+
   return (
     <main className="all-countries-page">
       <FilterButtons />
 
-      {countries.loading ? (
+      {api.loading || countries.data.length === 0 ? (
         <h2>Loading...</h2>
+      ) : !api.loading && countriesToDisplay.length === 0 ? (
+        <h2> No Countries match your search criteria</h2>
       ) : countries.error ? (
         <h2>Error, could not get countries...</h2>
-      ) : countriesToDisplay.length === 0 ? (
-        <h2> No Countries match your search criteria</h2>
       ) : (
         <AllCountriesContainer countries={countriesToDisplay} />
       )}

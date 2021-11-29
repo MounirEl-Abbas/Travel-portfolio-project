1a63e9485c1631ccfde84270ce3d00888f82b691
Mounir El-Abbas <eamounir1@gmail.com>
2021-11-28 19:54:59 -0500

    Edit navbar jsx for styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,16 +5,18 @@ import { LogoIpsum } from "../assets/";
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={LogoIpsum} alt="" />
-      <section className="nav-routes-container">
+    <nav className="navbar">
+      <section className="navbar-logo-container">
+        <img src={LogoIpsum} alt="" />
+      </section>
+      <section className="navbar-routes-container">
         <Link to="/">Home</Link>
         <Link to="/countries">Explore</Link>
         <HashLink to="/#pricing-section">Pricing</HashLink>
         <Link to="/about">About</Link>
       </section>
-      <section className="user-btns">
-        <button>Login</button>
+      <section className="navbar-log-btns">
+        <button>Try for free!</button>
         <button>Sign Up</button>
       </section>
     </nav>

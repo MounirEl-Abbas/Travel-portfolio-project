1ccc7184d62c387bb93a75572c7cacc13dbb2342
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-02 12:47:49 -0500

    Style-singleCountry-Header

--- src/components/singleCountry-Page/partials/CountryTicketMaster.js
+++ src/components/singleCountry-Page/partials/CountryTicketMaster.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 const CountryTicketMaster = ({ events, isLoading }) => {
-  console.log(`isLoading`, isLoading);
-  console.log(`events`, events);
-  if (isLoading) return <h2>Loading events...</h2>;
-  if (!events) return <h2>No events found, check again later...</h2>;
+  if (isLoading) return <h2 className="response">Loading events...</h2>;
+  if (!events)
+    return <h2 className="response">No events found, check again later...</h2>;
 
   return (
     <section className="ticketmaster-events">

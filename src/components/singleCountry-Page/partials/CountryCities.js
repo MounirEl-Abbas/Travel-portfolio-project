27cbedfdcb850a07c2451fad175ccf68d8fc5113
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-05 13:49:52 -0500

    Animate singleCountry-Page

--- src/components/singleCountry-Page/partials/CountryCities.js
+++ src/components/singleCountry-Page/partials/CountryCities.js
@@ -6,13 +6,17 @@ const CountryCities = ({ cities, setCurrentCity, countryName }) => {
   const dispatch = useDispatch();
   return (
     <section className="country-cities">
-      <section className="cities-title">
+      <section className="cities-title" data-aos="fade-down">
         <h2>Choose a city to visit in {countryName}</h2>
       </section>
       <section className="cities-container">
         {cities.map((city) => {
           return (
-            <article className="city">
+            <article
+              className="city"
+              data-aos="fade-up"
+              data-aos-duration="2000"
+            >
               <Link
                 to={`${city.cityName}`}
                 onClick={() => dispatch(setCurrentCity(city))}

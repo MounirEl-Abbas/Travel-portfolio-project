28006c1e4a0ebdc2aabca62b428640728b5f3ccf
Mounir El-Abbas <eamounir1@gmail.com>
2021-12-05 13:47:57 -0500

    Install aos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //components
@@ -13,7 +14,13 @@ import Footer from "./components/Footer";
 
 import ScrollToTop from "./utils/ScrollToTop";
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const App = () => {
+  useEffect(() => {
+    AOS.init({});
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
